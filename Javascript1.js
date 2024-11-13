ac3dfb18dda85e73eb6a22d1675001dbccb900ea
Iamraygb <jesuisrayb@gmail.com>
2024-11-13 15:42:14 -0500

    Javascript lesson 1 practice

---
+++ Javascript1.js
@@ -0,0 +1,90 @@
+let currentage=20.6;
+console.log(currentage);
+
+let userName= "Ray b9";
+console.log(userName);
+
+let Happy= false;
+console.log(Happy);
+
+let House;
+console.log(House);
+
+let house=null;
+console.log(House);
+
+let Bignumber= 202399399383828882827272222222222222222222222222222222n;
+console.log(Bignumber);
+
+let Firstname="Ray";
+console.log(Firstname);
+
+let Lastname="Sama"
+
+console.log(Firstname,Lastname);
+
+let a=100;
+let b=20;
+
+//a +=10; other way to do it==>a =a +10;
+a=a+10;
+console.log(a+b, a*b, a-b, a % b);
+
+let c=100;
+let d=20;
+d=d+10;
+console.log(d);
+
+let e=100;
+let f="20";
+console.log(e==f, e!=f, e===f);
+
+//logical, and either or, logical inverse
+let g=true;
+let h=false;
+console.log(g && h, g || h, !g);
+ 
+//comparison operator
+let kitchen;
+let Middlename="Ghogomu";
+if(Middlename=="Ghogomu"){
+    kitchen="Ray"
+    console.log(kitchen)
+}
+let Crocodiles=10;
+let birds= 2;
+console.log(Crocodiles>=birds)
+
+let Red_balloon;
+let name="Brad";
+if (name=="Andrew")
+{ Red_balloon="Andrew"
+console.log(Red_balloon);
+}
+else{
+    Red_balloon="Invalid User"
+    console.log(Red_balloon);
+}
+
+let Crab;
+let crabname="Laughing crab" //User input
+if(crabname=="Snow crab"){
+    Crab="Type of crab"
+    console.log(Crab);
+}
+else if(crabname=="Giant mud crab"){
+    Crab="Type of crab";
+    console.log(Crab);
+}
+else{
+    Crab="Not a crab name"
+    console.log(Crab)
+}
+
+let age=18; //user input;
+let status= age>= 18 ? "You're an adult": "You're a minor";
+console.log(status)
+
+let menu="fries";
+let availability=menu=="fries" ? "On the menu" : "Not on the Menu";
+console.log(availability)
